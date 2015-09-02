1f70d11d36a548e74e2eedcda86b6f91ceef8c33
Phil Vacca <phil.vacca@gmail.com>
2015-09-02 01:57:05 -0500

    add make_replica.sh

---
+++ make_replica.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TARGET="$1"
+PRIMARY="$2"
+
+PGDATA=/pgdata/ppas-9.4
+sed -i -e 's/primary\.host/$PRIMARY/' recovery/recovery.conf
+scp recovery/recovery.conf root@$TARGET:$PGDATA/
+
+ssh root@$TARGET "sed -i -e 's/# hot_standby/hot_standby/' $PGDATA/postgresql.edb.conf"
+
+
