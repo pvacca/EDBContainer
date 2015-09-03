e5993320dafb5f3e7ee21b6d265388f2e01a72ca
Phil Vacca <phil.vacca@gmail.com>
2015-09-03 16:17:26 -0500

    Move install scripts to install_scripts.

--- install_ppas.sh
+++ install_ppas.sh
@@ -6,11 +6,11 @@ YUMPASS=""
 ENTERPRISEDB_PASS=""
 REPL_PASS=""
 
-ssh root@$TARGET 'bash -s' < configure_yum.sh $YUMUSER $YUMPASS
+ssh root@$TARGET 'bash -s' < install_scripts/configure_yum.sh $YUMUSER $YUMPASS
 ssh root@$TARGET 'yum install -y ppas94-server pem-agent'
-./copy_environment_files.sh $TARGET
-ssh root@$TARGET 'bash -s' < init_postgresql.sh $ENTERPRISEDB_PASS
-./copy_postgresql_conf.sh $TARGET
-ssh root@$TARGET 'bash -s' < create_extensions.sh
-ssh root@$TARGET 'bash -s' < create_repl_user.sh $REPL_PASS
+install_scripts/copy_environment_files.sh $TARGET
+ssh root@$TARGET 'bash -s' < install_scripts/init_postgresql.sh $ENTERPRISEDB_PASS
+install_scripts/copy_postgresql_conf.sh $TARGET
+ssh root@$TARGET 'bash -s' < install_scripts/create_extensions.sh
+ssh root@$TARGET 'bash -s' < install_scripts/create_repl_user.sh $REPL_PASS
 ssh root@$TARGET 'cat >> $PGDATA/pg_hba.conf' < pg_hba_conf/pg_hba.conf
