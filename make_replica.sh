f478904bed46a433b7f05a4d8782a9cc7c8e27b0
Phil Vacca <phil.vacca@gmail.com>
2015-09-24 00:15:04 -0500

    Add REPL_PASSWORD

--- make_replica.sh
+++ make_replica.sh
@@ -2,9 +2,11 @@
 
 TARGET="$1"
 PRIMARY="$2"
+REPL_PASSWORD="$3"
 
 PGDATA=/pgdata/ppas-9.4
 scp recovery/recovery.conf root@$TARGET:$PGDATA/
 ssh root@$TARGET "sed -i -e 's/primary\.host/$PRIMARY/' $PGDATA/recovery.conf"
 ssh root@$TARGET "chown enterprisedb:enterprisedb $PGDATA/recovery.conf"
 ssh root@$TARGET "sed -i -e 's/# hot_standby/hot_standby/' $PGDATA/postgresql.edb.conf"
+ssh root@$TARGET "echo *:5432:repl:replication:$REPL_PASSWORD >> ~enterprisedb/.pgpass"
