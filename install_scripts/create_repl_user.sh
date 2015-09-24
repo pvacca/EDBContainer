951522609181f2e75ae1f2b5f3b74d32ec5a079b
Phil Vacca <phil.vacca@gmail.com>
2015-09-23 21:43:39 -0500

    replication user and installation

--- install_scripts/create_repl_user.sh
+++ install_scripts/create_repl_user.sh
@@ -2,7 +2,10 @@
 
 REPL_PASSWORD="$1"
 
-service ppas-9.4 start
+if [ ! -n "$(service ppas-9.4 status |grep running)" ]; then
+	service ppas-9.4 start
+fi
+
 edb-psql -U enterprisedb edb -q -c "CREATE ROLE repl REPLICATION LOGIN ENCRYPTED PASSWORD '$REPL_PASSWORD';"
 echo "*:5432:replication:repl:$REPL_PASSWORD" >> ~enterprisedb/.pgpass
 
--- make_replica.sh
+++ make_replica.sh
@@ -4,7 +4,7 @@ TARGET="$1"
 PRIMARY="$2"
 
 PGDATA=/pgdata/ppas-9.4
-sed -i -e 's/primary\.host/$PRIMARY/' recovery/recovery.conf
 scp recovery/recovery.conf root@$TARGET:$PGDATA/
+ssh root@$TARGET "sed -i -e 's/primary\.host/$PRIMARY/' $PGDATA/recovery.conf"
 ssh root@$TARGET "chown enterprisedb:enterprisedb $PGDATA/recovery.conf"
 ssh root@$TARGET "sed -i -e 's/# hot_standby/hot_standby/' $PGDATA/postgresql.edb.conf"
