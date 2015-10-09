f6992db778627020e0a93ed5be8d207ee01b7058
Phil Vacca <phil.vacca@gmail.com>
2015-10-08 21:13:28 -0500

    remove dots from ~enterprisedb/dotfiles

--- install_scripts/copy_environment_files.sh
+++ install_scripts/copy_environment_files.sh
@@ -2,7 +2,7 @@
 TARGET="$1"
 
 scp -p sysconfig/ppas-9.4 root@$TARGET:/etc/sysconfig/ppas/ppas-9.4
-scp -p -r enterprisedb_user/bash_profile root@$TARGET:~enterprisedb/.bash_profile
-scp -p -r enterprisedb_user/psqlrc root@$TARGET:~enterprisedb/.psqlrc
+scp -p enterprisedb_user/bash_profile root@$TARGET:~enterprisedb/.bash_profile
+scp -p enterprisedb_user/psqlrc root@$TARGET:~enterprisedb/.psqlrc
 ssh root@$TARGET 'touch ~enterprisedb/.pgpass && chmod 0600 ~enterprisedb/.pgpass'
 ssh root@$TARGET 'chown -R enterprisedb:enterprisedb ~enterprisedb/'
