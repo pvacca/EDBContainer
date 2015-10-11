26da5fe84b63a54f0350d8a90bd4e104b0173651
Phil Vacca <phil.vacca@gmail.com>
2015-10-11 01:55:04 -0500

    ignore links to conf files

--- docker/ppas-volume/entrypoint.sh
+++ docker/ppas-volume/entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 mkdir -p $PGDATA
 mkdir -p $PGXLOG
 mkdir -p $PGLOG
-# mkdir -p /var/run/ppas-$PG_MAJOR
+
 chown enterprisedb:enterprisedb $PGDATA
 chown enterprisedb:enterprisedb $PGXLOG
 chown enterprisedb:enterprisedb $PGLOG
+
 chown enterprisedb:enterprisedb /var/run/ppas-$PG_MAJOR
+
+runuser -l enterprisedb -c "exec '$@'"
