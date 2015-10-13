014dba1dd49eeae13cd9db17ea23afa758b2c88e
Phil Vacca <phil.vacca@gmail.com>
2015-10-13 02:51:50 -0500

    Fun with volumes

--- docker/ppas-volume/entrypoint.sh
+++ docker/ppas-volume/entrypoint.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 set -e
 
-mkdir -p $PGDATA && chown -R enterprisedb:enterprisedb $PGDATA
-mkdir -p $PGXLOG && chown -R enterprisedb:enterprisedb $PGXLOG
-mkdir -p $PGLOG && chown -R enterprisedb:enterprisedb $PGLOG
+# Start commands are: data-only, xlog-only, dataxlog, log-only, all
+case "$1" in
+"all")
+  shift
+  mkdir -p $PGDATA && chown -R enterprisedb:enterprisedb $PGDATA
+  mkdir -p $PGXLOG && chown -R enterprisedb:enterprisedb $PGXLOG
+  mkdir -p $PGLOG && chown -R enterprisedb:enterprisedb $PGLOG
+  ;;
+"dataxlog")
+  shift
+  mkdir -p $PGDATA && chown -R enterprisedb:enterprisedb $PGDATA
+  mkdir -p $PGXLOG && chown -R enterprisedb:enterprisedb $PGXLOG
+  ;;
+"data-only")
+  shift
+  mkdir -p $PGDATA && chown -R enterprisedb:enterprisedb $PGDATA
+  ;;
+"xlog-only")
+  shift
+  mkdir -p $PGXLOG && chown -R enterprisedb:enterprisedb $PGXLOG
+  ;;
+"log-only")
+  shift
+  mkdir -p $PGLOG && chown -R enterprisedb:enterprisedb $PGLOG
+  ;;
+esac
 
 runuser -l enterprisedb -c "exec '$@'"
