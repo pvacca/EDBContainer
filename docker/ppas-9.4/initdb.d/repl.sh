cb9c33383489fe9210bfc547d50e68c93730cd24
Phil Vacca <phil.vacca@gmail.com>
2015-10-20 14:50:10 -0500

    Replication startup script

---
+++ docker/ppas-9.4/initdb.d/repl.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+REPL_PASSWORD=${REPL_PASSWORD:-"repl-password"}
+
+edb-psql edb -q -c "CREATE ROLE repl REPLICATION LOGIN ENCRYPTED PASSWORD '$REPL_PASSWORD';"
+echo "*:$PGPORT:replication:repl:$REPL_PASSWORD" >> ~enterprisedb/.pgpass
+echo "host replication repl 0.0.0.0/0 md5" >> $PGDATA/pg_hba.conf
