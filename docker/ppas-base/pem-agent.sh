1529128ae4a34745b688a4c5df684965690feb1b
Phil Vacca <pvacca@users.noreply.github.com>
2016-01-17 22:45:31 -0600

    correct sudoers grammar (# is not a comment)

--- docker/ppas-base/pem-agent.sh
+++ docker/ppas-base/pem-agent.sh
@@ -8,7 +8,7 @@ chown -R enterprisedb:enterprisedb $PEM_AGENT
 mkdir ~enterprisedb/.pem && chown enterprisedb:enterprisedb ~enterprisedb/.pem
 
 # grant permission for enterprisedb to start pem agent as root
-echo "includedir /etc/sudoers.d" >>/etc/sudoers
+echo "#includedir /etc/sudoers.d" >>/etc/sudoers
 mkdir /etc/sudoers.d
 cat > /etc/sudoers.d/01enterprisedb <<-EOF
 Cmnd_Alias PEM_AGENT = $PEM_AGENT/bin/pemagent, $PEM_AGENT/bin/pkgLauncher
